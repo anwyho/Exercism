c7510764cf24c61f77047caedf8e2b2b75f4c4f0
Anthony Ho <anwyho@gmail.com>
2021-10-30 01:16:41 -0700

    Update SpaceAge

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -2,16 +2,18 @@ module SpaceAge (Planet(..), ageOn) where
 
 data Planet = Mercury | Venus | Earth | Mars | Jupiter | Saturn | Uranus | Neptune
 
--- Not my best work, but it works and it's 1am. 
-ageOn :: Planet -> Float -> Float
+secondsToEarthYears :: Fractional a => a -> a
+secondsToEarthYears s = s / 31557600
+
+ageOn :: Fractional a => Planet -> a -> a
 ageOn planet seconds = case planet of
-    Mercury -> yearsOldInSecondsOfOrbitalPeriod seconds 0.2408467
-    Venus   -> yearsOldInSecondsOfOrbitalPeriod seconds 0.61519726
-    Earth   -> yearsOldInSecondsOfOrbitalPeriod seconds 1.0000
-    Mars    -> yearsOldInSecondsOfOrbitalPeriod seconds 1.8808158
-    Jupiter -> yearsOldInSecondsOfOrbitalPeriod seconds 11.862615
-    Saturn  -> yearsOldInSecondsOfOrbitalPeriod seconds 29.447498
-    Uranus  -> yearsOldInSecondsOfOrbitalPeriod seconds 84.016846
-    Neptune -> yearsOldInSecondsOfOrbitalPeriod seconds 164.79132
-    where yearsOldInSecondsOfOrbitalPeriod s ratio = s / 31557600 / ratio
+    Mercury -> earthYears / 0.2408467
+    Venus   -> earthYears / 0.61519726
+    Earth   -> earthYears / 1.0000
+    Mars    -> earthYears / 1.8808158
+    Jupiter -> earthYears / 11.862615
+    Saturn  -> earthYears / 29.447498
+    Uranus  -> earthYears / 84.016846
+    Neptune -> earthYears / 164.79132
+    where earthYears = secondsToEarthYears seconds
     
